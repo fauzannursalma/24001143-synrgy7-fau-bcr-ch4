9beaef6731c8ec85e4b49655cc41fbc297de6fe7
Fauzan Nursalma <55134999+fauzannursalma@users.noreply.github.com>
2024-05-03 20:46:35 +0700

    fixing mini bug

--- public/scripts/car.js
+++ public/scripts/car.js
@@ -42,30 +42,30 @@ class Car {
   render() {
     return `
       <div class="flex flex-col w-[333px] mx-auto p-6 bg-white border rounded-lg shadow-sm">
-      <div class="w-full h-56  md:w-72 mb-3" style="
-        background: url(${this.image});
-        background-size: cover;
-        background-position: center;
-      "></div>
-      <p class="font-semibold text-sm mb-2">${this.manufacture} ${this.model}</p>
-      <p class="font-bold text-base mb-2">Rp ${this.rentPerDay} / hari</p>
-      <p class="font-light text-sm mb-3">${this.description}</p>
-      <ul class="font-light text-sm leading-2 flex flex-wrap gap-4 mb-6">
-        <li class="flex w-full items-center">
-          <i data-feather="users" class="text-neutral-03 stroke-[1px] mr-2"></i>
-          ${this.capacity} orang
-        </li>
-        <li class="flex w-full items-center">
-          <i data-feather="settings" class="text-neutral-03 stroke-[1px] mr-2"></i>
-          ${this.transmission}
-        </li>                
-        <li class="flex w-full items-center">
-          <i data-feather="calendar" class="text-neutral-03 stroke-[1px] mr-2"></i>
-          Tahun ${this.year}
-        </li>
-      </ul>
-      <button class="bg-limegreen w-full h-12 font-bold text-sm text-white mt-auto">Pilih Mobil</button>
-    </div>
+        <div class="w-full h-56  md:w-72 mb-3" style="
+          background: url(${this.image});
+          background-size: cover;
+          background-position: center;
+        "></div>
+        <p class="font-semibold text-sm mb-2">${this.manufacture} ${this.model}</p>
+        <p class="font-bold text-base mb-2">Rp ${this.rentPerDay} / hari</p>
+        <p class="font-light text-sm mb-3">${this.description}</p>
+        <ul class="font-light text-sm leading-2 flex flex-wrap gap-4 mb-6">
+          <li class="flex w-full items-center">
+            <i data-feather="users" class="text-neutral-03 stroke-[1px] mr-2"></i>
+            ${this.capacity} orang
+          </li>
+          <li class="flex w-full items-center">
+            <i data-feather="settings" class="text-neutral-03 stroke-[1px] mr-2"></i>
+            ${this.transmission}
+          </li>                
+          <li class="flex w-full items-center">
+            <i data-feather="calendar" class="text-neutral-03 stroke-[1px] mr-2"></i>
+            Tahun ${this.year}
+          </li>
+        </ul>
+        <button class="bg-limegreen w-full h-12 font-bold text-sm text-white mt-auto">Pilih Mobil</button>
+      </div>
     `;
   }
 }
